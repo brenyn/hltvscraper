4f00b864e5f9262f07cca4441d5df01370ff00a6
Brenyn <brenynk@gmail.com>
2020-05-14 20:53:14 -0400

    turned tablescraper into a function

--- tablescraper.py
+++ tablescraper.py
@@ -20,33 +20,39 @@
 import requests
 from bs4 import BeautifulSoup
 
-gamecount = 0
 
-games = []
-listofkeys = ['date','event','opponent','mapPlayed','result']
 
-url = "https://hltv.org/stats/teams/matches/9963/Infinity"
-r = requests.get(url)
+testurl = "https://hltv.org/stats/teams/matches/9963/Infinity"
 
-soup = BeautifulSoup(r.text, 'html.parser')
+def tablescraper (url):
+	gamecount = 0
+	
+	games = []
+	listofkeys = ['date','event','opponent','mapPlayed','result']
 
-# only interested in results table, save results to tablesoup variable
-tablesoup = soup.tbody
+	r = requests.get(url)
+	
+	soup = BeautifulSoup(r.text, 'html.parser')
+	
+	# only interested in results table, save results to tablesoup variable
+	tablesoup = soup.tbody
+			
+	# find all tr elements, each element is a unique game.
+	# note: not using css selectors because there are 2 different tr elements used for some reason.
+	# group-2 first and group-1 first. Seems more efficient to just find all tr elements.
+	gameTable = tablesoup.find_all('tr') 
+			
+	for game in gameTable:
+		games.append(dict.fromkeys(listofkeys,None))
+		cells = game.find_all('td')
+		games[gamecount]['date'] = (cells[0].text.strip())
+		games[gamecount]['event'] = (cells[1].text.strip())
+		games[gamecount]['opponent'] = (cells[3].text.strip())
+		games[gamecount]['mapPlayed'] = (cells[4].text.strip())
+		games[gamecount]['result'] = (cells[5].text.strip())
+		gamecount+=1	
 
-# find all tr elements, each element is a unique game.
-# note: not using css selectors because there are 2 different tr elements used for some reason.
-# group-2 first and group-1 first. Seems more efficient to just find all tr elements.
-gameTable = tablesoup.find_all('tr') 
-
-for game in gameTable:
-	games.append(dict.fromkeys(listofkeys,None))
-	cells = game.find_all('td')
-	games[gamecount]['date'] = (cells[0].text.strip())
-	games[gamecount]['event'] = (cells[1].text.strip())
-	games[gamecount]['opponent'] = (cells[3].text.strip())
-	games[gamecount]['mapPlayed'] = (cells[4].text.strip())
-	games[gamecount]['result'] = (cells[5].text.strip())
-	gamecount+=1
+	return games
 
 
 
