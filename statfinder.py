8be5d82fc5fc6c3c18d0998e91dfde2651f065f3
Brenyn <brenynk@gmail.com>
2020-05-16 02:31:40 -0400

    cleaned up notes to self

--- statfinder.py
+++ statfinder.py
@@ -6,15 +6,11 @@
 # Purpose: Prototype that navigates from match page to 2 teams stat pages
 # Input(s): URL to match
 # Output(s): URLs to team stats
-#
-# Note to self: breaking for teams with numbers in name
 #########################################################################################
 
 import requests
 from bs4 import BeautifulSoup
 from datetime import date
-
-testMatchURL = "https://www.hltv.org/matches/2341368/swedish-canadians-vs-infinity-esea-mdl-season-34-north-america"
 
 def statfinder (matchURL):
 	startDate = '2020-02-16'
@@ -29,16 +25,16 @@ def statfinder (matchURL):
 	
 	team1Name = team1Soup.find("div",class_="teamName").text.strip()
 	team2Name = team2Soup.find("div",class_="teamName").text.strip()
-	
+
 	team1ID = team1Soup.find("img",class_="logo").get('src')
 	team1ID = ''.join(filter(str.isdigit,team1ID))
 	team2ID = team2Soup.find("img",class_="logo").get('src')
 	team2ID = ''.join(filter(str.isdigit,team2ID))
-	
+
 	team1URL = team1Soup.find("a").get('href')
 	team2URL = team2Soup.find("a").get('href')
-	
+
 	team1URL = 'https://www.hltv.org/stats/teams/matches/' + team1ID + '/' + team1Name + timeFilter
 	team2URL = 'https://www.hltv.org/stats/teams/matches/' + team2ID + '/' + team2Name + timeFilter
-	
+
 	return (team1URL, team2URL)
--- tablescraper.py
+++ tablescraper.py
@@ -8,48 +8,38 @@
 # Output(s): TBD
 #
 # Notes to self:
-#
-# 1. Alternatively could find each team's HLTV ranking from team page and use that to sort
-# the teams. I like this idea better actually. Do this instead. Would make it much easier
-# to sort data later and is less janky/subject to break if they update the site.
-#
-# 2. Add opposing team rank to the list of keys, maybe home team name to make easier to sort
-#
-# 3. figure out how to export games data
+# 1. Add home team name to list of keys for sorting purposes
 #########################################################################################
 
 import requests
 from bs4 import BeautifulSoup
 
-urlList = ["https://www.hltv.org/stats/teams/matches/8772/Syman?startDate=2020-04-16&endDate=2020-05-16","https://www.hltv.org/stats/teams/matches/7969/Nemiga?startDate=2020-04-16&endDate=2020-05-16"]
+def tablescraper (url):
+	gamecount = 0
+	
+	games = []
+	listofkeys = ['date','event','opponent','mapPlayed','result']
 
-def tablescraper (teamstats):
-	for url in teamstats:
-		gamecount = 0
-		
-		games = []
-		listofkeys = ['date','event','opponent','mapPlayed','result']
-		
-		r = requests.get(url)
-		
-		soup = BeautifulSoup(r.text, 'html.parser')
-		
-		# only interested in results table, save results to tablesoup variable
-		tablesoup = soup.tbody
-		
-		# find all tr elements, each element is a unique game.
-		# note: not using css selectors because there are 2 different tr elements used for some reason.
-		# group-2 first and group-1 first. Seems more efficient to just find all tr elements.
-		gameTable = tablesoup.find_all('tr')
-		
-		for game in gameTable:
-			games.append(dict.fromkeys(listofkeys,None))
-			cells = game.find_all('td')
-			games[gamecount]['date'] = (cells[0].text.strip())
-			games[gamecount]['event'] = (cells[1].text.strip())
-			games[gamecount]['opponent'] = (cells[3].text.strip())
-			games[gamecount]['mapPlayed'] = (cells[4].text.strip())
-			games[gamecount]['result'] = (cells[5].text.strip())
-			gamecount+=1
-		
-		print (games)#print games for test, need to export to csv/excel/google sheets here
+	r = requests.get(url)
+	
+	soup = BeautifulSoup(r.text, 'html.parser')
+	
+	# only interested in results table, save results to tablesoup variable
+	tablesoup = soup.tbody
+	
+	# find all tr elements, each element is a unique game.
+	# note: not using css selectors because there are 2 different tr elements used for some reason.
+	# group-2 first and group-1 first. Seems more efficient to just find all tr elements.
+	gameTable = tablesoup.find_all('tr') 
+	
+	for game in gameTable:
+		games.append(dict.fromkeys(listofkeys,None))
+		cells = game.find_all('td')
+		games[gamecount]['date'] = (cells[0].text.strip())
+		games[gamecount]['event'] = (cells[1].text.strip())
+		games[gamecount]['opponent'] = (cells[3].text.strip())
+		games[gamecount]['mapPlayed'] = (cells[4].text.strip())
+		games[gamecount]['result'] = (cells[5].text.strip())
+		gamecount+=1	
+
+	return games
